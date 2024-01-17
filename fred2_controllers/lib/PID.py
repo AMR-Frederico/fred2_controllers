852752064cd7f8ce3ae8f3f7fe85c4e3b0d7fbbb
Leticia <leticiarp2000@hotmail.com>
2024-01-17 19:41:07 -0300

    chore: add lib for PID controller

---
+++ fred2_controllers/lib/PID.py
@@ -0,0 +1,86 @@
+#!/user/bin/env python3
+
+from time import time 
+
+class PID_controller: 
+
+    def __init__(self, KP, KI, KD) -> None:
+
+        self.KP = KP
+        self.KI = KI
+        self.KD = KD
+
+        self.SATURATION = 10        # Saturation for integrative
+
+        self.time = time()
+        self.last_time = time()
+        self.delta_time = 0
+
+        self.error = 0
+        self.last_error = 0
+        self.delta_error = 0
+
+        self.integrative = 0
+
+
+
+    def proporcional(self): 
+
+        return self.KP * self.error
+    
+
+
+    def integrative(self): 
+
+        self.integrative += self.error * self.delta_time
+
+
+        if abs(self.integrative) > self.SATURATION: 
+
+            self.integrative = 0
+        
+        return self.integrative * self.KI
+    
+
+
+    def derivative(self):
+
+        self.delta_error = self.error - self.last_error
+
+        
+        if self.delta_error != 0: 
+
+            self.delta_error = self.delta_error / self.delta_time
+
+        else: 
+
+            self.delta_error = 0
+
+
+        return self.delta_error * self.KD 
+
+
+    def output(self, current_value, setpoint): 
+
+        self.error = setpoint - current_value 
+
+
+        self.time = time()
+        self.delta_time = self.time - self.last_time 
+
+
+        if self.error != 0: 
+            
+            pid_output = self.proporcional() + self.integrative() + self.derivative()
+
+        else: 
+
+            pid_output = self.proporcional() + self.derivative()
+
+        
+        self.last_error = self.error
+        self.last_time = self.time
+
+
+        return pid_output
+
