5ab2b09d49ac47a4787aafaef707ef971f168205
Leticia <leticiarp2000@hotmail.com>
2024-01-26 18:54:09 -0300

    chore: new configs for the python structure

--- fred2_controllers/positionController.py
+++ fred2_controllers/positionController.py
@@ -4,12 +4,13 @@ import rclpy
 import transforms3d as tf3d     # angle manipulaton 
 import threading 
 import math
+import os 
+import yaml 
 
 from typing import List
 
 from fred2_controllers.lib.PID import PID_controller
 from fred2_controllers.lib.quat_multiply import quaternion_multiply, reduce_angle
-from fred2_controllers.lib.load_params import load_params
 
 from rclpy.context import Context
 
@@ -24,13 +25,13 @@ from std_msgs.msg import Int16
 
 
 # Parameters file (yaml)
-node_path = '~/ros2_ws/src/fred2_controllers/conf/controllers_params.yaml'
+node_path = '/home/ubuntu/ros2_ws/src/fred2_controllers/config/controllers_params.yaml'
 node_group = 'position_control'
 
 
 
 
-class positionControl (Node): 
+class positionController (Node): 
     
     odom_pose = Pose()
 
@@ -47,25 +48,28 @@ class positionControl (Node):
 
     cmd_vel = Twist()
 
+    rotation_quat = [0.0, 0.0, 0.0, 0.0]
+    robot_quat = [0.0, 0.0, 0.0, 0.0]
+
     def __init__(self, 
-                 node_name: str, 
-                 *, # keyword-only argument
-                 context: Context = None, 
-                 cli_args: List[str] = None, 
-                 namespace: str = None, 
-                 use_global_arguments: bool = True, 
-                 enable_rosout: bool = True, 
-                 start_parameter_services: bool = True, 
-                 parameter_overrides: List[Parameter] | None = None) -> None:
+                node_name: str, 
+                *, # keyword-only argument
+                context: Context = None, 
+                cli_args: List[str] = None, 
+                namespace: str = None, 
+                use_global_arguments: bool = True, 
+                enable_rosout: bool = True, 
+                start_parameter_services: bool = True, 
+                parameter_overrides: List[Parameter] | None = None) -> None:
         
-        super().__init__(node_name, 
-                         context=context, 
-                         cli_args=cli_args, 
-                         namespace=namespace, 
-                         use_global_arguments=use_global_arguments, 
-                         enable_rosout=enable_rosout, 
-                         start_parameter_services=start_parameter_services, 
-                         parameter_overrides=parameter_overrides)
+        super().__init__(node_name=node_name, 
+                        context=context, 
+                        cli_args=cli_args, 
+                        namespace=namespace, 
+                        use_global_arguments=use_global_arguments, 
+                        enable_rosout=enable_rosout, 
+                        start_parameter_services=start_parameter_services, 
+                        parameter_overrides=parameter_overrides)
         
         
         # quality protocol -> the node must not lose any message 
@@ -77,27 +81,45 @@ class positionControl (Node):
 
 
         self.create_subscription(Odometry,
-                                 '/odom', 
-                                 self.odom_callback, 
-                                 qos_profile)
+                                '/odom', 
+                                self.odom_callback, 
+                                qos_profile)
         
 
         self.create_subscription(PoseStamped, 
-                                 '/goal_manager/goal/current', 
-                                 self.goalCurrent_callback, 
-                                 qos_profile)
+                                '/goal_manager/goal/current', 
+                                self.goalCurrent_callback, 
+                                qos_profile)
         
         self.create_subscription(Int16, 
-                                 '/machine_states/robot_state', 
-                                 self.robotState_callback, 
-                                 qos_profile)
+                                '/machine_states/robot_state', 
+                                self.robotState_callback, 
+                                qos_profile)
         
         self.vel_pub = self.create_publisher(Twist, 
                                             '/cmd_vel', 
                                             qos_profile)
         
-        load_params(node_path, node_group)
+
+        self.load_params(node_path, node_group)
         self.get_params()
+
+
+    def load_params(self, path, group): 
+        param_path = os.path.expanduser(path)
+
+        with open(param_path, 'r') as params_list: 
+            params = yaml.safe_load(params_list)
+        
+        # Get the params inside the specified group
+        params = params.get(group, {})
+
+        # Declare parameters with values from the YAML file
+        for param_name, param_value in params.items():
+            # Adjust parameter name to lowercase
+            param_name_lower = param_name.lower()
+            self.declare_parameter(param_name_lower, param_value)
+            self.get_logger().info(f'{param_name_lower}: {param_value}')
 
 
 
@@ -125,9 +147,9 @@ class positionControl (Node):
         self.goal_pose.y = goal.pose.position.y 
         
         self.goal_pose.theta = tf3d.euler.quat2euler([goal.pose.orientation.w, 
-                                                      goal.pose.orientation.x, 
-                                                      goal.pose.orientation.y, 
-                                                      goal.pose.orientation.z])[2]
+                                                    goal.pose.orientation.x, 
+                                                    goal.pose.orientation.y, 
+                                                    goal.pose.orientation.z])[2]
 
 
 
@@ -147,18 +169,30 @@ class positionControl (Node):
     
     def move_backward(self): 
 
-        reverse_facing = tf3d.euler.euler2quat(0, 0, math.pi)
+        rotation_180_degree_to_quat = tf3d.euler.euler2quat(0, 0, math.pi)   # Quaternion in w, x, y z (real, then vector) format
         
-        backwart_quat = Quaternion()
-        backwart_quat = quaternion_multiply(self.odom_pose.orientation, reverse_facing)
+        self.rotation_quat[0] = rotation_180_degree_to_quat[1]
+        self.rotation_quat[1] = rotation_180_degree_to_quat[2]
+        self.rotation_quat[2] = rotation_180_degree_to_quat[3]
+        self.rotation_quat[3] = rotation_180_degree_to_quat[0]
+
+        self.robot_quat[0] = self.odom_pose.orientation.x 
+        self.robot_quat[1] = self.odom_pose.orientation.y 
+        self.robot_quat[2] = self.odom_pose.orientation.z
+        self.robot_quat[3] = self.odom_pose.orientation.w 
+
+
+        
+        backwart_quat = []
+        backwart_quat = quaternion_multiply(self.robot_quat, self.rotation_quat)
 
         backwart_pose = Pose2D()
         backwart_pose.x = self.odom_pose.position.x 
         backwart_pose.y = self.odom_pose.position.y 
-        backwart_pose.theta = tf3d.euler.quat2euler([backwart_quat.w, 
-                                                     backwart_quat.x, 
-                                                     backwart_quat.y, 
-                                                     backwart_quat.z])[2]
+        backwart_pose.theta = tf3d.euler.quat2euler([backwart_quat[3], 
+                                                    backwart_quat[0], 
+                                                    backwart_quat[1], 
+                                                    backwart_quat[2]])[2]
 
 
         return backwart_pose
@@ -175,9 +209,9 @@ class positionControl (Node):
         front_pose.x = self.odom_pose.position.x 
         front_pose.y = self.odom_pose.position.y 
         front_pose.theta = tf3d.euler.quat2euler([front_quat.w, 
-                                                  front_quat.x, 
-                                                  front_quat.y, 
-                                                  front_quat.z])
+                                                front_quat.x, 
+                                                front_quat.y, 
+                                                front_quat.z])[2]
         
 
         return front_pose
@@ -242,10 +276,8 @@ class positionControl (Node):
             self.vel_pub.publish(self.cmd_vel)
 
 
-
-
-if __name__ == '__main__':
-
+def main(): 
+    
     # Create a custom context for single thread and real-time execution
     rclpy.init()
 
@@ -253,20 +285,19 @@ if __name__ == '__main__':
     position_context.init()
     position_context.use_real_time = True
     
-
-    node = positionControl(
-        node_name='position_control',
+    node = positionController(
+        node_name='positionController',
         context=position_context,
         cli_args=['--debug'],
         namespace='controllers',
         enable_rosout=False
     )
 
-    # Make the execution in real time 
+    # Make the execution in real-time 
     executor = SingleThreadedExecutor(context=position_context)
     executor.add_node(node)
 
-    # create a separate thread for the callbacks and another for the main function 
+    # Create a separate thread for the callbacks and another for the main function 
     thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
     thread.start()
 
@@ -274,11 +305,14 @@ if __name__ == '__main__':
 
     try: 
         while rclpy.ok(): 
-
             rate.sleep()
+            node.position_control()
         
     except KeyboardInterrupt:
         pass
 
     rclpy.shutdown()
     thread.join()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'fred2_controllers'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    packages=[package_name],
+    install_requires=[
+        'setuptools',
+        'rclpy',
+        'transforms3d',  # Add other dependencies as needed
     ],
-    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
     maintainer_email='leticiarp2000@hotmail.com',
-    description='TODO: Package description',
+    description='ROS 2 package for position control',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'positionController = fred2_controllers.positionController:main',
         ],
     },
 )
