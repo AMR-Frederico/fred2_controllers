7d55d2cffd592cea003100451904987f8fb77f57
Leticia <leticiarp2000@hotmail.com>
2024-01-27 14:42:46 -0300

    chore: config for the launch

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import os
 from setuptools import setup
+from glob import glob
 
 package_name = 'fred2_controllers'
 
@@ -22,4 +24,9 @@ setup(
             'positionController = fred2_controllers.positionController:main',
         ],
     },
+    data_files=[
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),],
 )
