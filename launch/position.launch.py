beb018c9811094964c12f730539d938eb9ab7e34
Leticia <leticiarp2000@hotmail.com>
2024-02-01 10:47:58 -0300

    style: code indentation

--- launch/position.launch.py
+++ launch/position.launch.py
@@ -5,7 +5,9 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    
     position_controller_node = Node(
+        
         package='fred2_controllers',
         executable='positionController',
         name='positionController',
