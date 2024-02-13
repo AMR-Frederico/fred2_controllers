0f876ddc91e2c3a5a20f63b336cf8018bc638064
LeticiaRP <leticiarp2000@hotmail.com>
2024-02-13 19:00:38 -0300

    fix: operation

--- fred2_controllers/lib/quat_multiply.py
+++ fred2_controllers/lib/quat_multiply.py
@@ -42,9 +42,9 @@ def quaternion_multiply(q0, q1):
 
 def reduce_angle(angle):
     while angle > math.pi:
-        angle -= 2*math.pi
+        angle = angle - 2*math.pi
     
     while angle <= -math.pi:
-        angle += 2*math.pi
+        angle = angle + 2*math.pi
     
     return angle
