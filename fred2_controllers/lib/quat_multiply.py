fix: orientation error in quaternion multipy lib

fred2_controllers/lib/quat_multiply.py
@@ -1,43 +1,27 @@
 #!/user/bin/env python3
 
-import numpy as np 
+import numpy
 import math 
 
-def quaternion_multiply(q0, q1):
-    """
-    Multiplies two quaternions.
 
-    Input
-    :param q0: A 4 element array containing the first quaternion (q01, q11, q21, q31)
-    :param q1: A 4 element array containing the second quaternion (q02, q12, q22, q32)
 
-    Output
-    :return: A 4 element array containing the final quaternion (q03,q13,q23,q33)
+def quaternion_multiply(quaternion1, quaternion0):
+    """Return multiplication of two quaternions.
+
+    >>> q = quaternion_multiply([1, -2, 3, 4], [-5, 6, 7, 8])
+    >>> numpy.allclose(q, [-44, -14, 48, 28])
+    True
 
     """
-    # Extract the values from q0
-    w0 = q0[0]
-    x0 = q0[1]
-    y0 = q0[2]
-    z0 = q0[3]
+    x0, y0, z0, w0 = quaternion0
+    x1, y1, z1, w1 = quaternion1
+    return numpy.array((
+         x1*w0 + y1*z0 - z1*y0 + w1*x0,
+        -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+         x1*y0 - y1*x0 + z1*w0 + w1*z0,
+        -x1*x0 - y1*y0 - z1*z0 + w1*w0), dtype=numpy.float64)
 
-    # Extract the values from q1
-    w1 = q1[0]
-    x1 = q1[1]
-    y1 = q1[2]
-    z1 = q1[3]
 
-    # Computer the product of the two quaternions, term by term
-    q0q1_w = w0 * w1 - x0 * x1 - y0 * y1 - z0 * z1
-    q0q1_x = w0 * x1 + x0 * w1 + y0 * z1 - z0 * y1
-    q0q1_y = w0 * y1 - x0 * z1 + y0 * w1 + z0 * x1
-    q0q1_z = w0 * z1 + x0 * y1 - y0 * x1 + z0 * w1
-
-    # Create a 4 element array containing the final quaternion
-    final_quaternion = np.array([q0q1_w, q0q1_x, q0q1_y, q0q1_z])
-
-    # Return a 4 element array containing the final quaternion (q02,q12,q22,q32)
-    return final_quaternion
 
 
 def reduce_angle(angle):